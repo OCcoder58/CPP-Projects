9d23a6d26992efc840a6c7dde2e4c2de57f1a4f9
Oguzhan C. <ocankatar58@gmail.com>
2026-03-25 03:22:32 +0100

    Implement main function to test mystring class

--- project1/main.cpp
+++ project1/main.cpp
@@ -1,1 +1,16 @@
 
+#include <iostream>
+#include "newString.h"
+
+int main()
+{
+    mystring::String a("Hallo");
+    mystring::String b(nullptr);
+    mystring::String c;
+
+    std::cout << a.getString() << " (" << a.getLength() << ")\n";
+    std::cout << b.getString() << " (" << b.getLength() << ")\n";
+    std::cout << c.getString() << " (" << c.getLength() << ")\n";
+
+    return 0;
+}
